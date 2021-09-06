31e462ed7e6c83ecd1e6c8e98c60e59edc214508
M-Dnny <mansuridanish14@gmail.com>
2021-09-06 21:42:27 +0530

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_login/models/user.dart';
 import 'package:firebase_login/screens/wrapper.dart';
 import 'package:firebase_login/services/auth.dart';
 import 'package:flutter/material.dart';
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,7 +2,8 @@ import 'package:firebase_login/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-  const SignIn({Key? key}) : super(key: key);
+  final Function toggleView;
+  const SignIn({required this.toggleView});
 
   @override
   _SignInState createState() => _SignInState();
@@ -10,31 +11,58 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+
+  // TextField State
+  String email = '';
+  String password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown,
+      backgroundColor: Colors.brown[100],
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
         title: Text('Sign In'),
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 50,
-          vertical: 20,
-        ),
-        child: ElevatedButton(
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null) {
-              print("Sign In did not Worked");
-            } else {
-              print('Sign In Worked');
-              print(result.uid);
-            }
-          },
-          child: Text('Guest Sign In'),
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: Form(
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              TextFormField(
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  print(email);
+                  print(password);
+                },
+                child: Text(
+                  'Sign In',
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.pink[400],
+                ),
+              ),
+              SizedBox(height: 10),
+              TextButton(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  child: Text("Don't have an account? Register Now"))
+            ],
+          ),
         ),
       ),
     );
